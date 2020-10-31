f81021189d626a3866932063f7c9b58c6c65c9d8
William Tomé <william.tomenh@gmail.com>
2020-10-31 16:52:04 -0300

    Adicionado serviço do Google.

--- config/services.php
+++ config/services.php
@@ -34,6 +34,12 @@ return [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => env('FACEBOOK_URL_CALLBACK'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_URL_CALLBACK'),
     ]
 
 ];
