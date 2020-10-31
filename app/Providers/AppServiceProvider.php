b9c777132271f8b036fce63474a39217d78d0cf3
William Tomé <william.tomenh@gmail.com>
2020-10-31 11:14:42 -0300

    Binding das classes de provider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use App\Services\SocialUserResolver;
+use Coderello\SocialGrant\Resolvers\SocialUserResolverInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public $bindings = [
+        SocialUserResolverInterface::class => SocialUserResolver::class
+    ];
+
     /**
      * Register any application services.
      *
