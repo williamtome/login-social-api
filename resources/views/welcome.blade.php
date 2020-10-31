799ebf6f70400557b718bd1ab52c6725fb949635
William Tomé <william.tomenh@gmail.com>
2020-10-31 12:17:40 -0300

    Botão para efetuar login com o Facebook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,36 @@
         </style>
     </head>
     <body>
+
+    <script>
+        window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1277519422586502',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v8.0'
+        });
+
+        FB.AppEvents.logPageView();
+
+        };
+
+        (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+
+        function checkLoginState() {
+            FB.getLoginStatus(function(response) {
+                console.log(response);
+                statusChangeCallback(response);
+            });
+        }
+    </script>
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -78,6 +108,11 @@
                     @endauth
                 </div>
             @endif
+
+            <fb:login-button
+                scope="public_profile,email"
+                onlogin="checkLoginState();">
+            </fb:login-button>
 
             <div class="content">
                 <div class="title m-b-md">
